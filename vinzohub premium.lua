027af83ed2c60c3ed30daeda2f2c35f6bf4383a8
Vinzoo801 <finono620@gmail.com>
2026-03-29 22:03:53 +0700

    🔑 Add key VNZ-9A2FB886C50144C8 | Exp: 05/04/2026

--- vinzohub premium.lua
+++ vinzohub premium.lua
@@ -30,7 +30,8 @@ end
 -- =====================================================
 local KEY_DATABASE = {
 	-- key akan otomatis ditambah oleh bot Discord
-	["VNZ-B1C1824DCA0F44C2"] = { expired = "01/04/2026", lockedUser = nil },
+	["VNZ-B1C1824DCA0F44C2"] = { expired = "01/04/2026", lockedUser = nil 	["VNZ-9A2FB886C50144C8"] = { expired = "05/04/2026", lockedUser = nil },
+},
 }
 
 -- =====================================================
