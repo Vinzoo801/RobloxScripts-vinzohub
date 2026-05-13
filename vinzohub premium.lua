ccaccb7299f55cfa81786caeabe37c493992698c
Vinzoo801 <finono620@gmail.com>
2026-05-13 19:49:46 +0700

    Update vinzohub premium.lua

--- vinzohub premium.lua
+++ vinzohub premium.lua
@@ -72,7 +72,7 @@ end
 -- =====================================================
 local KEY_DATABASE = {
 	-- key akan otomatis ditambah oleh bot Discord
-	["VNZ-4D294132F7C64DAD"] = { expired = "02/04/2026", lockedUser = nil },
+	["VNZ-4D294132F7C64DAD"] = { expired = "15/05/2026", lockedUser = nil },
 }
 
 local function isExpired(expiredStr)
